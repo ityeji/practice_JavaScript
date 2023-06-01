f8ced43e6a161c6d98f0ba005adefb3e9b30c79a
Jeong Yeji <wjddpwl1222@gmail.com>
2023-06-01 11:59:31 +0900

    01/06/23 Study JavaScript Object

---
+++ TodoList/script.js
@@ -0,0 +1,19 @@
+const calculator = {
+    add: function(a, b){
+        console.log(a + b);
+    },
+    minus: function(a, b){
+        console.log(a - b);
+    },
+    div: function(a, b){
+        console.log(a / b);
+    },
+    powerof: function(a, b){
+        console.log(a ** b);
+    },
+};
+
+calculator.add(2, 5);
+calculator.minus(2, 5);
+calculator.div(2, 5);
+calculator.powerof(2, 5);
